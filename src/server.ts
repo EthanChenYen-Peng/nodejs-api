93ab0397001dc3a2c7991f2c7784d3338648e5b8
EthanChenYen-Peng <ypcethan@gmail.com>
2022-04-19 15:54:29 +1000

    add task routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express'
+import taskRoutes from './routes/task.routes'
 
 const PORT = 3000
 const app = express()
 
+app.use('/api/v1/tasks', taskRoutes)
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello form server')
 })
