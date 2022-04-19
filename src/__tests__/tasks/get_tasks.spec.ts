c6e86d42cf07b5d0dfab581d61d448f918ad310b
EthanChenYen-Peng <ypcethan@gmail.com>
2022-04-19 17:08:38 +1000

    setup testing environment

--- src/__tests__/tasks/get_tasks.spec.ts
+++ src/__tests__/tasks/get_tasks.spec.ts
@@ -1,9 +1,28 @@
 import supertest, { Response } from 'supertest'
 import { app } from '../../server'
+import { prisma } from '../../utils/prisma'
+
+beforeEach(async () => {
+  await prisma.task.deleteMany()
+})
 
 describe('GET /api/v1/tasks', () => {
+  beforeEach(async () => {
+    await prisma.task.create({
+      data: {
+        title: 'Task 1',
+        description: 'asdf',
+      },
+    })
+  })
   it('returns ok', async () => {
     const response: Response = await supertest(app).get('/api/v1/tasks')
     expect(response.statusCode).toBe(200)
+  })
+
+  it('returns tasks', async () => {
+    const response: Response = await supertest(app).get('/api/v1/tasks')
+    const taskData = response.body
+    expect(taskData['tasks']).toHaveLength(1)
   })
 })
--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,9 +1,12 @@
 import { Request, Response, Router } from 'express'
+import { prisma } from '../utils/prisma'
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Get all tasks')
+router.get('/', async (req: Request, res: Response) => {
+  const tasks = await prisma.task.findMany()
+
+  res.json({ tasks })
 })
 
 export default router
